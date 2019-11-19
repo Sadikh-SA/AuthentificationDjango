cc196b1e1c0b2f7ef20d275e71ff896fd0f20872
Sadikh-SA <abougueye96@yahoo.fr>
2019-11-19 12:35:53 +0000

    authentification fait

--- authentification/urls.py
+++ authentification/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 from connexion import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.HelloView.as_view(), name='hello'),
+    path('login', obtain_auth_token, name='login'),
 ]
 
